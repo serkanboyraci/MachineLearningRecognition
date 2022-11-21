b3c81d381045254e28eb33a5503ba65f63ed07b3
serkanboyraci <serkanboyraci@gmail.com>
2022-11-21 10:12:14 +0300

    Initial Commit

---
+++ MachineLearningRecognation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningRecognation
+//
+//  Created by Ali serkan Boyracı  on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
